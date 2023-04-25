019c2f6d3ff51307242d142f996149565032f1aa
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-25 14:11:48 -0300

    Get from users ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Form from './components/Form.js'
 import Grid from './components/Grid.js'
 import {toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Container = styled.div`
   width: 100%;
@@ -20,12 +22,28 @@ const Title = styled.h2`
 `
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [onEdit, setOnEdit] = useState(null)
+
+  const getUsers = async () => {
+    try{
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)))
+    } catch (error) {
+      toast.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [setUsers])
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
         <Form />
-        <Grid />
+        <Grid users={users}/>
       </Container>
 
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
