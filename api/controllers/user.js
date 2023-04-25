16a36125e55c1f73b2ca1e4cfe6117653186ce01
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-25 14:23:30 -0300

    Creation of CRUD methods

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -11,3 +11,54 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data)
     })
 }
+
+export const addUser = (req, res) => {
+    const q = "INSERT INTO usuarios(`nome`, `email`, `telefone`, `data_nascimento`) VALUES(?)"
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.telefone,
+        req.body.data_nascimento,
+    ]
+
+    db.query(q, [values], (err) => {
+        if(err){
+            return res.json(err)
+        }
+
+        return res.status(200).json("Usuário cadastrado com sucesso.")
+    })
+}
+
+
+export const updateUser = (req, res) => {
+    const q = "UPDATE usuarios SET `nome` = ?, `email` = ?, `telefone` = ?, `data_nascimento` = ? WHERE `id` = ?"
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.telefone,
+        req.body.data_nascimento,
+    ]
+
+    db.query(q, [...values, res.params.id], (err) => {
+        if(err){
+            return res.json(err)
+        }
+
+        return res.status(200).json("Usuário atualizado com sucesso.")
+    })
+}
+
+export const deleteUser = (req, res) => {
+    const q = "DELETE FROM usuarios WHERE `id` = ?"
+
+    db.query(q, [req.params.id], (err) => {
+        if(err){
+            return res.json(err)
+        }
+
+        return res.status(200).json("Usuário deletado com sucesso.")
+    })
+}
